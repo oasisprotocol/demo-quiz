9b7e2b92d2dcb0bbc639107d03d040a79805464a
ZigaMr <ziga.mrzljak@gmail.com>
2024-07-18 01:17:51 +0200

    Added separate NFT contract for minting ERC721 tokens, removed linter, kept frontend changes
    Cosmetic changes
    
    Add SVG base64 image to NFT contract state, tests for OasisReward contract, update hardhat.config.ts and fix CI/CD workflows.
    
    Add automatically generated base64 image to NFT contract state, tests for NFT-OasisReward contract, update hardhat.config to include NFT deploy and setup
    
    Add separate tests for Rewards contract, increase gasless tx gasLimit parameter, add docBlock comments
    
    Add CI/CD update, cherry-pick whitelist commit from backup branch.
    
    Cosmetic changes
    
    Fix tests for CI/CD
    
    Update ci-test.yaml
    
    Add SVG base64 image to NFT contract state, tests for OasisReward contract, update hardhat.config.ts and fix CI/CD workflows.
    
    Add automatically generated base64 image to NFT contract state, tests for NFT-OasisReward contract, update hardhat.config to include NFT deploy and setup
    
    Add separate tests for Rewards contract, increase gasless tx gasLimit parameter, add docBlock comments
    
    Add CI/CD update, cherry-pick whitelist commit from backup branch.
    
    Cosmetic changes
    
    Fix tests for CI/CD
    
    Update ci-test.yaml
    
    Add Chain ID check to tests, add reward flags for NFT/ROSE
    
    Fix frontend/package.json
    
    Remove pnpm-lock.yaml
    
    Remove vscode settings, add natspec

---
+++ backend/contracts/OasisReward.sol
@@ -0,0 +1,120 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+
+/**
+ * @title OasisReward
+ * @dev OasisReward contract is an ERC721 contract that allows minting NFT rewards for Oasis Quiz
+ */
+contract OasisReward is ERC721Enumerable { 
+
+    mapping(uint256 => string) private _tokenURIs;
+
+    /// @notice Mapping from user address to list of owned token IDs
+    mapping(address => uint256[]) private _ownedTokens;
+    /// @notice Whitelisted msg.sender accounts
+    mapping(address => bool) _allowMint;
+
+
+    /// @notice Contract owner
+    address private _owner;
+
+    
+    modifier onlyOwner {
+        require(msg.sender == _owner, "Only owner can call this function");
+        _;
+    }
+
+    modifier onlyAllowMint {
+        require(_allowMint[msg.sender], "Address not allowed");
+        _;
+    }
+
+    constructor(
+        string memory name,
+        string memory symbol
+    ) ERC721(name, symbol) {
+        _owner = msg.sender;
+    }
+
+
+    function addAllowMint(address _address) public onlyOwner {
+        _allowMint[_address] = true;
+    }
+
+    function removeAllowMint(address _address) public onlyOwner {
+        _allowMint[_address] = false;
+    }
+
+    /**
+     * @notice Mint new NFT reward
+     * @param to address to mint the NFT to
+     * @param base64EncodedSVG base64 encoded SVG image
+     */
+    // Function to mint new tokens
+    function mint(address to, string calldata base64EncodedSVG) public onlyAllowMint {
+        require(bytes(base64EncodedSVG).length > 0, "Token URI not set");
+        uint256 tokenId = totalSupply();
+        _tokenURIs[tokenId] = base64EncodedSVG;
+        _safeMint(to, tokenId);
+        _ownedTokens[to].push(tokenId); // Add the tokenId to the list of owned tokens for the address 'to'
+    }
+
+    /**
+     * 
+     * @notice Generate a simple SVG image that can be stored on-chain
+     * @param quizID unique identifier for the quiz contract
+     */
+    function generateComplexSVG(
+        uint32 quizID
+    ) public view onlyAllowMint returns (string memory) {
+
+        // Use tokenId to generate some unique details for the SVG
+        // For simplicity, let's change the circle's color based on the tokenId's parity
+        string memory circleColor = quizID % 2 == 0 ? "blue" : "red";
+
+        // SVG parts concatenated with dynamic data
+        string memory svg = string(abi.encodePacked(
+            "<svg width=\"200\" height=\"200\" xmlns=\"http://www.w3.org/2000/svg\">",
+            "<circle cx=\"100\" cy=\"100\" r=\"50\" fill=\"", circleColor, "\" />",
+            "</svg>"
+        ));
+
+        // Encode the entire SVG string to Base64
+        string memory base64EncodedSVG = Base64.encode(bytes(svg));
+
+        // Return the data URI for the SVG image
+        return
+            string(
+                abi.encodePacked("data:image/svg+xml;base64,", base64EncodedSVG)
+            );
+    }
+
+    /**
+     * 
+     * @notice Returns the SVG base64 encoded string for a tokenID
+     * @param tokenId unique identifier for the token
+     * 
+     */
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual override returns (string memory) {
+        require(
+            _exists(tokenId),
+            "ERC721URIStorage: URI set of nonexistent token"
+        );
+        return _tokenURIs[tokenId];
+    }
+
+    /**
+     * 
+     * @notice Returns the list of token IDs owned by an address
+     * @param owner address of the owner
+     */
+    function getOwnedTokens(address owner) public view returns (uint256[] memory) {
+        require(owner != address(0), "Address cannot be the zero address.");
+        return _ownedTokens[owner];
+    }
+}
