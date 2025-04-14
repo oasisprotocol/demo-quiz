cc5f193e3d115efc0f593e7a9b82a6880792eb96
Kaja <kaja.osojnik@gmail.com>
2025-04-14 17:00:47 +0100

    Add english translations

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -1,18 +1,15 @@
 <script setup lang="ts">
+import { useI18n } from "vue-i18n";
 const currentYear = new Date().getFullYear();
+const { t } = useI18n();
 </script>
 
 <template>
   <footer class="text-xs w-full absolute bottom-5 px-8">
-    <p class="text-white text-center">
-      Copyright © {{ currentYear }}
-      <a href="https://oasisprotocol.org" target="_blank"
-        >Oasis Protocol Foundation</a
-      >. Licensed under
-      <a href="https://www.apache.org/licenses/LICENSE-2.0" target="_blank"
-        >Apache 2.0</a
-      >.
-    </p>
+    <p
+      class="text-white text-center"
+      v-html="t('copyright', { year: currentYear })"
+    ></p>
   </footer>
 </template>
 
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
-
+import { useI18n } from "vue-i18n";
 import { useQuiz } from "../contracts";
 import { useEthereumStore } from "../stores/ethereum";
 import AppButton from "@/components/AppButton.vue";
 
+const { t } = useI18n();
 const eth = useEthereumStore();
 const quiz = useQuiz();
 
@@ -31,16 +32,9 @@ onMounted(async () => {
     <h1
       class="text-white text-center font-['Roboto_Mono'] text-2xl md:text-3xl mb-5 leading-snug max-w-md m-auto tracking-wider"
     >
-      Welcome to the Oasis blockchain quiz!🌹
+      {{ $t("greeting") }}
     </h1>
-    <p class="text-white text-center mb-7">
-      You probably know everything about the bitcoin and the Ethereum
-      blockchains, but there are also other chains out there such as the
-      <a href="https://oasisprotocol.org" target="_blank">Oasis Network</a>! ⛓
-      This quiz runs on Oasis Sapphire inside a
-      <b>confidential smart contract</b>, and verifies your answers completely
-      on-chain. 🔒
-    </p>
+    <p class="text-white text-center mb-7" v-html="t('quiz_intro')"></p>
     <div class="form-group">
       <input
         type="text"
@@ -55,7 +49,7 @@ onMounted(async () => {
         for="couponText"
         class="peer-focus:text-primaryDark peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8 peer-focus:text-white"
       >
-        Enter your coupon:
+        {{ $t("enter_coupon") }}
         <span class="text-red-500">*</span>
       </label>
     </div>
@@ -64,29 +58,18 @@ onMounted(async () => {
       :to="{ name: 'quiz', params: { coupon: quizCoupon } }"
       class="flex no-underline"
     >
-      <AppButton variant="primary" class="m-auto hover:text-white hover:bg-primaryRoyal hover:border-white">Confirm</AppButton>
+      <AppButton
+        variant="primary"
+        class="m-auto hover:text-white hover:bg-primaryRoyal hover:border-white"
+        >{{ $t("confirm") }}</AppButton
+      >
     </RouterLink>
 
-    <p class="text-white mt-5 mb-20" v-if="isReward">
-      When you solve the quiz, you will receive a <b>1 ROSE</b> reward! You can
-      use the tokens to
-      <a href="https://docs.oasis.io/dapp/sapphire/quickstart" target="_blank"
-        >develop a dApp</a
-      >
-      🔨 and deploy it on the sapphire-testnet. If you don't have an idea for
-      your dApp, perhaps you will find an inspiration by checking out our
-      <a href="https://playground.oasis.io/" target="_blank">playground</a
-      >.💡<br /><br />
-      Of course, you can also exchange it for other tokens on a
-      <a href="https://illuminex.xyz" target="_blank">DEX</a>. Maybe get you and
-      your friends a <s>beer</s> hot chocolate in the nearby
-      <a href="https://map.bitcoin.com/" target="_blank">crypto pub</a>? ☕ Or
-      you can just hodl your tokens and maybe their value will surge. 🤑<br /><br />
-
-      In either case, join us on
-      <a href="https://oasis.io/discord" target="_blank">Discord</a>. You will
-      find a lot of interesting answers there. Good luck!😉
-    </p>
+    <p
+      class="text-white mt-5 mb-20"
+      v-if="isReward"
+      v-html="t('reward_note')"
+    ></p>
   </section>
 </template>
 
